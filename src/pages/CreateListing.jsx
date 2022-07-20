c388667ba8e3e52dd6b4166fe8f69be7e2448548
Ruoying Jiang <rhoswen827@gmail.com>
2022-07-20 03:50:02 -0400

    edit listing page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -14,6 +14,7 @@ import { v4 as uuidv4 } from "uuid";
 import Spinner from "../components/Spinner";
 
 function CreateListing() {
+  // eslint-disable-next-line
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
@@ -61,6 +62,7 @@ function CreateListing() {
     });
 
     return unsub;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [auth, navigate]);
 
   const onSubmit = async (e) => {
@@ -132,6 +134,8 @@ function CreateListing() {
                 break;
               case "running":
                 console.log("Upload is running");
+                break;
+              default:
                 break;
             }
           },
